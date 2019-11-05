05dd4d14374796e83311143ea32fad28c4f1c3a0
Angel <pixan-angel@pixan.io>
2019-11-05 10:13:22 -0600

    fixed previous commit

--- src/core/db.ts
+++ src/core/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
-    if (![0, 3].includes(mongoose.connection.readyState)) {
+    if (![1, 2].includes(mongoose.connection.readyState)) {
         await mongoose.connect(`${process.env.db_connection_string}/${process.env.db_name}`, {
             autoIndex: true,
             useNewUrlParser: true,
